06d13dc78f6ce3939646c2110f8ad538b78ac635
Özgür Boruzancı <ozgurboruzanc@gmail.com>
2024-04-01 03:35:19 +0300

    Card transactions and betting system

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+[Serializable]
+public class Card : MonoBehaviour, IComparable
+{
+    public CardValue cardValue;
+    public CardColor cardColor;
+
+    public int CompareTo(object obj)
+    {
+        if (obj is Card)
+        {
+            return this.cardValue.CompareTo((obj as Card).cardValue);
+        }
+        else
+        {
+            throw new ArgumentException("Object to compare is not a Card");
+        }
+    }
+}
